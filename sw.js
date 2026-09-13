7be3dca13cc65391708270afce04e7628c1fa0ba
juhyeon <ljh1869@gmail.com>
2026-09-13 23:38:18 +0900

    Keep settlement seat map centered after login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v13';
+const CACHE_VERSION = 'v14';
 const CACHE_PREFIX = 'hongcal-';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const LEGACY_CACHE_PREFIXES = ['aebaeryeok-'];
