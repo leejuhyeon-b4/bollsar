cbbdbbf5a1b48d8a1aa1e1d5e52359278737f089
juhyeon <ljh1869@gmail.com>
2026-09-15 17:45:11 +0900

    Align visited seat borders and floor dividers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v26';
+const CACHE_VERSION = 'v27';
 const CACHE_PREFIX = 'hongcal-';
 const CACHE_NAME = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const LEGACY_CACHE_PREFIXES = ['aebaeryeok-'];
